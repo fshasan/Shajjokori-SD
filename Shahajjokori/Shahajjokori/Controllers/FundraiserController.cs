a87edd43c5220b1393d8ba885342b30e5c190f3b
Abrar Rafid Noor <arn.official@yahoo.com>
2021-03-04 09:10:14 +0600

    main site has been worked

--- Shahajjokori/Shahajjokori/Controllers/FundraiserController.cs
+++ Shahajjokori/Shahajjokori/Controllers/FundraiserController.cs
@@ -52,8 +52,6 @@ namespace Shahajjokori.Controllers
                 e.e_photo.CopyTo(new FileStream(filePath, FileMode.Create));
             }
             
-            
-            
             var fr = JsonConvert.DeserializeObject<Fundraiser>(HttpContext.Session.GetString("FundraiserSession"));
         
             string connection_string = configuration.GetConnectionString("DefaultConnectionString");
@@ -78,12 +76,51 @@ namespace Shahajjokori.Controllers
             com.Parameters.AddWithValue("@category", e.e_category);
 
             com.ExecuteNonQuery();
-            //ViewData["Total_fundraiser"] = count;
+            ViewData["e_key"] = uniqueFileName.ToString();
             connection.Close();
             //return RedirectToAction("Create_event_entry","Fundraiser");
             return View(e);
         }
 
+        /*public IActionResult Create_payment_entry(Payment p, string event_key)
+        {
+            //var fr = JsonConvert.DeserializeObject<Fundraiser>(HttpContext.Session.GetString("FundraiserSession"));
+            string connection_string = configuration.GetConnectionString("DefaultConnectionString");
+            SqlConnection connection = new SqlConnection(connection_string);
+            connection.Open();
+            string query = "Select e_id from EVENT where e_pic = @pic";
+            SqlCommand com = new SqlCommand(query, connection);
+
+            com.Parameters.AddWithValue("@pic", event_key);
+            //com.Parameters.AddWithValue("@password", fundraiser.f_password);
+
+            //string connection_string = configuration.GetConnectionString("DefaultConnectionString");
+            //SqlConnection connection = new SqlConnection(connection_string);
+            //connection.Open();
+            ////string query = "SELECT [f_id],[f_name],[f_email],[f_password],[f_phone],[f_about],[f_category] FROM [dbo].[FUNDRAISERS]"
+
+            //event pic is neglected for the time being
+            string query = "INSERT INTO [dbo].[EVENT] ([e_title],[e_location],[e_opening_date],[e_closing_date],[e_exp_amount],[e_pic],[e_raised_amount],[e_donor_count],[e_state],[e_details],[f_id],[e_category]) VALUES (@title,@location,@o_date,@c_date,@exp,@pic,0,0,0,@details,@f_id, @category)";
+            SqlCommand com = new SqlCommand(query, connection);
+            com.Parameters.AddWithValue("@title", e.e_title);
+            com.Parameters.AddWithValue("@location", e.e_location);
+            com.Parameters.AddWithValue("@o_date", e.e_opening_date);
+            com.Parameters.AddWithValue("@c_date", e.e_closing_date);
+            com.Parameters.AddWithValue("@exp", e.e_exp_amount);
+            //com.Parameters.AddWithValue("@raised", e.e_raised_amount);
+            //com.Parameters.AddWithValue("@count", e.e_donor_count);
+            com.Parameters.AddWithValue("@pic", uniqueFileName.ToString());
+            //com.Parameters.AddWithValue("@state", e.e_state);
+            com.Parameters.AddWithValue("@details", e.e_details);
+            com.Parameters.AddWithValue("@f_id", fr.f_id);
+            com.Parameters.AddWithValue("@category", e.e_category);
+
+            com.ExecuteNonQuery();
+            //ViewData["Total_fundraiser"] = count;
+            connection.Close();
+            //return RedirectToAction("Create_event_entry","Fundraiser");
+            return View(e);
+        }*/
         public IActionResult See_Events()
         {
             var fr = JsonConvert.DeserializeObject<Fundraiser>(HttpContext.Session.GetString("FundraiserSession"));
--- Shahajjokori/Shahajjokori/Controllers/HomeController.cs
+++ Shahajjokori/Shahajjokori/Controllers/HomeController.cs
@@ -48,13 +48,175 @@ namespace Shahajjokori.Controllers
         //{
         //    GetFundraisers = await connectionDB.FUNDRAISERS.ToListAsync(); 
         //}
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
         public IActionResult Index()
         {
-            return View();
+            string connection_string = configuration.GetConnectionString("DefaultConnectionString");
+            SqlConnection connection = new SqlConnection(connection_string);
+            connection.Open();
+            ////string query = "SELECT [f_id],[f_name],[f_email],[f_password],[f_phone],[f_about],[f_category] FROM [dbo].[FUNDRAISERS]"
+            //var id = fr.f_id;
+            string query = $"select TOP 3 * from EVENT";
+
+            SqlCommand com = new SqlCommand(query, connection);
+
+            var model = new List<Event>();
+            using (SqlConnection conn = new SqlConnection(connection_string))
+            {
+                conn.Open();
+                SqlDataReader rdr = com.ExecuteReader();
+                while (rdr.Read())
+                {
+                    var e = new Event();
+                    e.e_id = (int)rdr["e_id"];
+                    e.e_title = (string)rdr["e_title"];
+                    e.e_category = (int)rdr["e_category"];
+                    e.e_location = (string)rdr["e_location"];
+                    e.e_opening_date = (string)rdr["e_opening_date"];
+                    e.e_closing_date = (string)rdr["e_closing_date"];
+                    e.e_exp_amount = (int)rdr["e_exp_amount"];
+                    e.e_raised_amount = (int)rdr["e_raised_amount"];
+                    e.e_donor_count = (int)rdr["e_donor_count"];
+                    e.e_state = (int)rdr["e_state"];
+                    e.e_pic = (string)rdr["e_pic"];
+                    e.e_details = (string)rdr["e_details"];
+
+                    model.Add(e);
+                }
+
+            }
+
+            return View(model);
+            //return View();
         }
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+
+        public IActionResult Event_section(string option)
+        {
+            string connection_string = configuration.GetConnectionString("DefaultConnectionString");
+            SqlConnection connection = new SqlConnection(connection_string);
+            connection.Open();
+            if (option == "Education")
+            {
+                string query1 = $"select * from EVENT where e_category = 0";
+                SqlCommand com1 = new SqlCommand(query1, connection);
+
+                var model1 = new List<Event>();
+                using (SqlConnection conn = new SqlConnection(connection_string))
+                {
+                    conn.Open();
+                    SqlDataReader rdr = com1.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        var e = new Event();
+                        e.e_id = (int)rdr["e_id"];
+                        e.e_title = (string)rdr["e_title"];
+                        e.e_category = (int)rdr["e_category"];
+                        e.e_location = (string)rdr["e_location"];
+                        e.e_opening_date = (string)rdr["e_opening_date"];
+                        e.e_closing_date = (string)rdr["e_closing_date"];
+                        e.e_exp_amount = (int)rdr["e_exp_amount"];
+                        e.e_raised_amount = (int)rdr["e_raised_amount"];
+                        e.e_donor_count = (int)rdr["e_donor_count"];
+                        e.e_state = (int)rdr["e_state"];
+                        e.e_pic = (string)rdr["e_pic"];
+                        e.e_details = (string)rdr["e_details"];
+
+                        model1.Add(e);
+                    }
+
+                }
+
+                return View(model1);
+            }
+            //if (option == "All")
+            //{
+            //    string query = $"select * from EVENT";
+            //}
+
+            /*
+            SqlCommand com = new SqlCommand(query, connection);
+
+            var model = new List<Event>();
+            using (SqlConnection conn = new SqlConnection(connection_string))
+            {
+                conn.Open();
+                SqlDataReader rdr = com.ExecuteReader();
+                while (rdr.Read())
+                {
+                    var e = new Event();
+                    e.e_id = (int)rdr["e_id"];
+                    e.e_title = (string)rdr["e_title"];
+                    e.e_category = (int)rdr["e_category"];
+                    e.e_location = (string)rdr["e_location"];
+                    e.e_opening_date = (string)rdr["e_opening_date"];
+                    e.e_closing_date = (string)rdr["e_closing_date"];
+                    e.e_exp_amount = (int)rdr["e_exp_amount"];
+                    e.e_raised_amount = (int)rdr["e_raised_amount"];
+                    e.e_donor_count = (int)rdr["e_donor_count"];
+                    e.e_state = (int)rdr["e_state"];
+                    e.e_pic = (string)rdr["e_pic"];
+                    e.e_details = (string)rdr["e_details"];
+
+                    model.Add(e);
+                }
+
+            }*/
+
+            return View();
+            
+        }
+
+        [Route("Home/Event_Details/{id}")]
+        public IActionResult Event_Details(int id)
+        {
+            //Event e = new Event();
+            ViewBag.event_id = id;
+            string connection_string = configuration.GetConnectionString("DefaultConnectionString");
+            SqlConnection connection = new SqlConnection(connection_string);
+            connection.Open();
+            string query = $"Select * from EVENT where e_id = {id}";
+            SqlCommand com = new SqlCommand(query, connection);
+            SqlDataReader dr = com.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Event e = new Event();
+                e.e_title = (string)dr["e_title"];
+                ViewBag.event_cat = (int)dr["e_category"];
+                e.e_location = (string)dr["e_location"];
+                e.e_opening_date = (string)dr["e_opening_date"];
+                e.e_closing_date = (string)dr["e_closing_date"];
+                ViewBag.exp_amount = (int)dr["e_exp_amount"];
+                ViewBag.raised_amount = (int)dr["e_raised_amount"];
+                e.e_donor_count = (int)dr["e_donor_count"];
+                e.e_state = (int)dr["e_state"];
+                e.e_pic = (string)dr["e_pic"];
+                ViewBag.event_pic = (string)dr["e_pic"];
+                e.e_details = (string)dr["e_details"];
+                var f_id = (int)dr["f_id"];
+                connection.Close();
+
+                string connection_string2 = configuration.GetConnectionString("DefaultConnectionString");
+                SqlConnection connection2 = new SqlConnection(connection_string2);
+                connection2.Open();
+                string query2 = $"Select * from FUNDRAISERS where f_id = {f_id}";
+                SqlCommand com2 = new SqlCommand(query2, connection2);
+                SqlDataReader dr2 = com2.ExecuteReader();
+
+                while (dr2.Read()) {
+                    ViewBag.org = (string)dr2["f_name"];
+                    
+                }
+                connection2.Close();
+                return View(e);
+            }
             return View();
         }
         public IActionResult SignIn()
